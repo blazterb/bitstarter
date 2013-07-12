923f062e2bc40325bf180dba1c9ab7286dc4855f
blazterb <blaz.terbovc@gmail.com>
2013-07-12 20:01:50 +0000

    modified2 web.js

--- web.js
+++ web.js
@@ -2,8 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
+var buff = new Buffer(16);
+
+buff = fs.readFileSync('index.html')
+
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync(index.html, [string]));
+  response.send(buff.toString());
 });
 
 var port = process.env.PORT || 5000;
